b6b8ad23dfb16bf5bb4598df070792cb950136a8
alidev <aamansouri@gmail.com>
2019-11-04 21:55:53 -0500

    Load the WDBC dataset as pandas dataframe

---
+++ cls/data/WDBC.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def load_WDBC(return_X_y=False):
+    # Read the dataset 
+    featureNames_org = ['Radius', 'Texture', 'Perimeter', 'Area', 'Smoothness', 'Compactness', 'Concavity', 'Concave points', 'Symmetry', 'Fractal dimension']
+    featureNames = [f'{s} (M)' for s in featureNames_org]
+    featureNames += [f'{s} (SE)' for s in featureNames_org]
+    featureNames += [f'{s} (W)' for s in featureNames_org]
+    featureNames = ['ID', 'Diagnosis'] + featureNames
+
+    df = pd.read_csv('data/Breast Cancer Wisconsin/wdbc.data',
+        delimiter=',', header=None, names=featureNames)
+    # drop the ID column
+    df = df.drop(columns='ID')
+    # Map the targets from categorical (string) values to 0/1
+    diagnosis_mapping = {'B':0, 'M':1}
+    df['Diagnosis'] = df['Diagnosis'].map(diagnosis_mapping).astype(int)
+    return df
+# df = load_WDBC()
+# print(df.head())
+# df.info()
+# df.describe()
