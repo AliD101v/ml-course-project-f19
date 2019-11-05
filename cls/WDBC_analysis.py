ddd75240ea7b2e164e557b09d64fb53ddb362b11
alidev <aamansouri@gmail.com>
2019-11-05 16:12:37 -0500

    Add pipleline (preprocessing and fitting)

--- cls/WDBC_analysis.py
+++ cls/WDBC_analysis.py
@@ -9,29 +9,38 @@ import pandas as pd
 from pandas.plotting import scatter_matrix
 import numpy as np
 import sklearn
-from sklearn import preprocessing
 from sklearn.pipeline import Pipeline
+# preprocessing
+from sklearn import preprocessing
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.decomposition import PCA
-from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
+# model selection
+from sklearn.model_selection import train_test_split, GridSearchCV
+# metrics
 from sklearn import metrics
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
-from sklearn.ensemble import BaggingClassifier
-from sklearn import svm
-from sklearn.datasets import load_svmlight_file
+# estimators
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+# scipy
 import scipy
 from scipy.sparse import csr_matrix
 from IPython.core.display import display
+# plotting
 import plotly
 import plotly.graph_objects as go
 import plotly.express as px
 import matplotlib.pyplot as plt
+# misc
 import timeit
 from datetime import datetime
 
@@ -59,3 +68,108 @@ y = df[df.columns[0]]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size,
                                     random_state=random_seed)
 
+# ────────────────────────────────────────────────────────────────────────────────
+# # 3. Pipeline
+
+# ## 3.1 Transformers
+# ### 3.1.1 Continuous (quantitative or numeric) transformer
+# Example:
+# ```python
+# numeric_transformer = Pipeline(steps=[
+#     ('imputer', SimpleImputer(strategy='median')),
+#     ('scaler', StandardScaler())])
+# ```
+numeric_transformer = Pipeline(steps=[
+    ('scaler', StandardScaler())])
+# 
+# ### 3.1.2 Categorical (qualitative) transformer
+# Example:
+# ```python
+# categorical_transformer = Pipeline(steps=[
+#     ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+#     ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+# ```
+
+# ### 3.1.3 Column transformer
+# Example:
+# ```python
+# numeric_features = train.select_dtypes(include=['int64', 'float64']).columns
+# categorical_features = train.select_dtypes(include=['object']).drop(['Loan_Status'], axis=1).columns
+# 
+# preprocessor = ColumnTransformer(
+#     transformers=[
+#         ('num', numeric_transformer, numeric_features),
+#         ('cat', categorical_transformer, categorical_features)])
+# ```
+
+numeric_features = df.select_dtypes(include=['int64', 'float64']).columns
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features)])
+
+# ## 3.2 Classifier
+# Example:
+# ```python
+# rf = Pipeline(steps=[('preprocessor', preprocessor),
+#                       ('classifier', RandomForestClassifier())])
+# rf.fit(X_train, y_train)
+# y_pred = rf.predict(X_test)
+# ```
+
+
+# Example for model selection:
+# ```python
+# classifiers = [
+#     KNeighborsClassifier(3),
+#     SVC(kernel="rbf", C=0.025, probability=True),
+#     NuSVC(probability=True),
+#     DecisionTreeClassifier(),
+#     RandomForestClassifier(),
+#     AdaBoostClassifier(),
+#     GradientBoostingClassifier()
+#     ]for classifier in classifiers:
+#     pipe = Pipeline(steps=[('preprocessor', preprocessor),
+#                       ('classifier', classifier)])
+#     pipe.fit(X_train, y_train)   
+#     print(classifier)
+#     print("model score: %.3f" % pipe.score(X_test, y_test))
+# ```
+
+classifiers = [
+    LogisticRegression(),
+    KNeighborsClassifier(3),
+    GaussianNB(),
+    SVC(kernel="rbf", C=0.025, probability=True),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+    AdaBoostClassifier(),
+    MLPClassifier()]
+
+for classifier in classifiers:
+    pipe = Pipeline(steps=[('preprocessor', preprocessor),
+                    ('classifier', classifier)])
+    pipe.fit(X_train, y_train)   
+    print(classifier)
+    print("model score: %.3f" % pipe.score(X_test, y_test))
+    print('==================================================')
+
+# ## 3.3 Hyperparameter tuning
+
+
+# Exampel:
+# ```python
+# param_grid = { 
+#     'classifier__n_estimators': [200, 500],
+#     'classifier__max_features': ['auto', 'sqrt', 'log2'],
+#     'classifier__max_depth' : [4,5,6,7,8],
+#     'classifier__criterion' :['gini', 'entropy']}from sklearn.model_selection import GridSearchCVCV = GridSearchCV(rf, param_grid, n_jobs= 1)
+                  
+# CV.fit(X_train, y_train)  
+# print(CV.best_params_)    
+# print(CV.best_score_)
+# ```
+
+# # 4. Output
+# ## 4.1 Results
+# ## 4.1 Figures
