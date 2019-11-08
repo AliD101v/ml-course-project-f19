d052dbd586c94923e64f1fd351fad806e3d29ca0
alidev <aamansouri@gmail.com>
2019-11-07 22:25:20 -0500

    Minor edit

--- cls/data/WDBC.py
+++ cls/data/WDBC.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def load_WDBC():
     # Read the dataset 
+    # Prepare the feature names
     featureNames_org = ['Radius', 'Texture', 'Perimeter', 'Area', 'Smoothness', 'Compactness', 'Concavity', 'Concave points', 'Symmetry', 'Fractal dimension']
     featureNames = [f'{s} (M)' for s in featureNames_org]
     featureNames += [f'{s} (SE)' for s in featureNames_org]
@@ -13,8 +14,8 @@ def load_WDBC():
     # drop the ID column
     df = df.drop(columns='ID')
     # Map the targets from categorical (string) values to 0/1
-    diagnosis_mapping = {'B':0, 'M':1}
-    df['Diagnosis'] = df['Diagnosis'].map(diagnosis_mapping).astype(int)
+    prediction_mapping = {'B':0, 'M':1}
+    df['Diagnosis'] = df['Diagnosis'].map(prediction_mapping).astype(int)
     return df
 # df = load_WDBC()
 # print(df.head())
