97b797ac556e40615886adc036b018eef407b871
alidev <aamansouri@gmail.com>
2019-11-18 15:23:50 -0500

    Add loading data from the NOAA Weather Bellevue dataset

---
+++ cls/data/NOAAWeatherBellevue.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+def load_NOAAWeatherBellevue():
+    featureNames = ['Temperature','Dew Point','Sea Level Pressure','Visibility','Average Wind Speed','Maximum Sustained Wind Speed','Maximum Temperature','Minimum Temperature']
+    # Read the dataset
+    df_X = pd.read_csv('data/NOAA Weather Bellevue/NEweather_data.csv',
+        delimiter=',', header=None, names=featureNames)
+
+    df_y = pd.read_csv('data/NOAA Weather Bellevue/NEweather_class.csv',
+        delimiter=',', header=None, names=['Rain'])
+
+    return df_X, df_y
+
+# df_X,df_y = load_NOAAWeatherBellevue()
+# print('Features:')
+# print(df_X.head())
+# df_X.info()
+# df_X.describe()
+# print('Targets:')
+# print(df_y.head())
+# df_y.info()
+# df_y.describe()
