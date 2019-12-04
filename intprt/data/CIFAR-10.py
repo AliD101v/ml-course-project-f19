c164514b3fa7e099b30279ac3546966bf3a08c66
alidev <aamansouri@gmail.com>
2019-12-04 16:42:42 -0500

    Add CIFAR-10 dataset load

---
+++ intprt/data/CIFAR-10.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pickle
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def load_CIFAR10():
+    train = list()
+    for i in range(5):
+        obj = unpickle(f'data/CIFAR-10/data_batch_{i+1}')
+        train.append(obj)
+    test = unpickle(f'data/CIFAR-10/test_batch')
+
+    return train, test
+
+## To test:
+# train, test = load_CIFAR10()
+# n = 5
+# print(f'First {n} records in...')
+# for i in range(len(train)):
+#     print(f'batch {i+1}:')
+#     obj_data = train[i][b'data']
+#     obj_labels = train[i][b'labels']
+#     print(f'{obj_data[:n,:n]}')
+#     print(f'{obj_labels[:n]}')
+# print(f'Test data:')
+# obj_data = test[b'data']
+# obj_labels = test[b'labels']
+# print(f'{obj_data[:n,:n]}')
+# print(f'{obj_labels[:n]}')
