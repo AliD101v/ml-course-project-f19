79838a1df9d037c431f23d719a16cfa9719664ce
alidev <aamansouri@gmail.com>
2019-12-05 17:38:28 -0500

    Add CIFAR-10 CNN

---
+++ intprt/CIFAR10_CNN.py
@@ -0,0 +1,102 @@
+import numpy as np
+import sklearn
+from sklearn.tree import DecisionTreeClassifier
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from PIL import Image
+from data.CIFAR10 import *
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+#%% [markdown]
+# Load the dataset.
+X, y, X_test, y_test = load_CIFAR10(transform=True)
+
+#%% [markdown]
+# Transform to normalized-range tensors [-1, 1]
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+#%% [markdown]
+# Display some sample images
+# num_samples = 3
+# indices = np.random.randint(0, X.shape[0], num_samples)
+# images = list()
+# labels = list()
+# for i in range(num_samples):
+#     images.append(transform(Image.fromarray(X[i])))
+#     labels.append(classes[y[i]])
+
+# imshow(torchvision.utils.make_grid(images))
+# print(' '.join('%5s' % labels[j] for j in range(num_samples)))
+
+#%% [markdown]
+# Create the network, loss, and optimizer.
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+#%% [markdown]
+# Train the convolutional neural network.
+batch_size = 1
+inputs = np.zeros((batch_size,) + X.shape[1:])
+for epoch in range(2):  # loop over the dataset multiple times
+    
+    running_loss = 0.0
+    # for i in range(0, X.shape[0], batch_size):
+    for i in range(X.shape[0]):
+        inputs = torch.tensor(np.expand_dims(transform(Image.fromarray(X[i])), axis=0))
+        label = torch.from_numpy(np.array([y[i]]).astype(np.int64))
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, label)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
